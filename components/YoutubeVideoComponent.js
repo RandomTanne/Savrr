de4a383ad7e80fdb0f3ad6e220db179a5e17288e
Jannik Pulfer <jannik.pulfer@icloud.com>
2024-09-22 17:56:55 +0200

    Make drag n drop work

--- components/YoutubeVideoComponent.js
+++ components/YoutubeVideoComponent.js
@@ -1,15 +1,23 @@
 import Link from "next/link";
 import { useState } from "react";
+import {useDraggable} from '@dnd-kit/core';
 
-export default function YoutubeVideoComponent({videoUrl}) {
+export default function YoutubeVideoComponent({video, id}) {
     const [url, setUrl] = useState("https://img.youtube.com");
+    const {attributes, listeners, setNodeRef, transform} = useDraggable({
+        id: id
+    });
+    const style = transform ? {
+        transform: `translate3d(${video.position.x + transform.x}px, ${video.position.y + transform.y}px, 0)`,
+    } : {transform: `translate3d(${video.position.x}px, ${video.position.y}px, 0)`};
+
     useState(() => {
-        const videoId = new URL(videoUrl).searchParams.get("v");
+        const videoId = new URL(video.url).searchParams.get("v");
         setUrl(`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`)
     }, [])
     return (
-        <div>
-            <Link href={videoUrl} target="_blank">
+        <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
+            <Link href={video.url} target="_blank">
                 <img src={url} width={300}/>
             </Link>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import YoutubeVideoComponent from "../components/YoutubeVideoComponent";
+import { DndContext } from "@dnd-kit/core";
 
 export default function IndexPage() {
+    const [youtubeVideos, setYoutubeVideos] = useState([{url: "https://www.youtube.com/watch?v=3N5MlZg_Cdg", position: {x: 0, y: 0}}, {url: "https://www.youtube.com/watch?v=YiRMs5ZhcH4", position: {x: 0, y: 0}}]);
+
+    function onDragEnd(e) {
+        const youtubeVideo = youtubeVideos[e.active.id - 1];
+        youtubeVideo.position.x += e.delta.x;
+        youtubeVideo.position.y += e.delta.y;
+    }
+
     return (
-        <div>
-            <YoutubeVideoComponent videoUrl={"https://youtube.com"}></YoutubeVideoComponent>
-        </div>
+        <DndContext id="context" onDragEnd={onDragEnd}>
+            {youtubeVideos.map((youtubeVideo, i) => {
+                return <YoutubeVideoComponent video={youtubeVideo} id={i + 1} key={i}></YoutubeVideoComponent>
+            })}
+        </DndContext>
     )
 }
