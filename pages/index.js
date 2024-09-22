70347f01fcf0deccff6db9667b5a569ae9b8c8aa
Jannik Pulfer <jannik.pulfer@icloud.com>
2024-09-22 21:33:25 +0200

    Add tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import YoutubeVideoComponent from "../components/YoutubeVideoComponent";
 import { DndContext } from "@dnd-kit/core";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 
 export default function IndexPage() {
     const [youtubeVideos, setYoutubeVideos] = useState([{url: "https://www.youtube.com/watch?v=3N5MlZg_Cdg", position: {x: 0, y: 0}}, {url: "https://www.youtube.com/watch?v=YiRMs5ZhcH4", position: {x: 0, y: 0}}]);
@@ -22,10 +23,13 @@ export default function IndexPage() {
     }
 
     return (
-        <DndContext id="context" onDragEnd={onDragEnd}>
-            {youtubeVideos.map((youtubeVideo, i) => {
-                return <YoutubeVideoComponent video={youtubeVideo} id={i + 1} key={i}></YoutubeVideoComponent>
-            })}
-        </DndContext>
+        <>
+            <DndContext id="context" onDragEnd={onDragEnd}>
+                {youtubeVideos.map((youtubeVideo, i) => {
+                    return <YoutubeVideoComponent video={youtubeVideo} id={i + 1} key={i}></YoutubeVideoComponent>
+                })}
+            </DndContext>
+            <PlusCircleIcon width={60}></PlusCircleIcon>
+        </>
     )
 }
