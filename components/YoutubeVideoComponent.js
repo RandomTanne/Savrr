4350a61b75ee8b367180a46e64d26b3e3c4c4f42
Jannik Pulfer <jannik.pulfer@icloud.com>
2024-09-24 19:17:32 +0200

    Implement header and make styling changes

--- components/YoutubeVideoComponent.js
+++ components/YoutubeVideoComponent.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import {useDraggable} from '@dnd-kit/core';
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 export default function YoutubeVideoComponent({video, id}) {
     const [url, setUrl] = useState("https://img.youtube.com");
@@ -21,11 +22,12 @@ export default function YoutubeVideoComponent({video, id}) {
         setUrl(`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`)
     }, [])
     return (
-        <div ref={setNodeRef} style={style}>
+        <div ref={setNodeRef} style={style} className="p-3 rounded bg-gradient-to-b from-amber-300 via-amber-500 to-amber-300">
             <Link href={video.url} target="_blank">
                 <img src={url} width={300}/>
             </Link>
-            <div {...listeners} {...attributes}>Test hehe</div>
+            <div>Test hehe</div>
+            <Bars3Icon className="size-7 absolute bottom-1 right-1 bg-yellow-200 p-1 rounded-full" {...listeners} {...attributes}></Bars3Icon>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import YoutubeVideoComponent from "../components/YoutubeVideoComponent";
 import { DndContext } from "@dnd-kit/core";
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import HeaderComponent from "../components/HeaderComponent";
 
 export default function IndexPage() {
     const [youtubeVideos, setYoutubeVideos] = useState([{url: "https://www.youtube.com/watch?v=3N5MlZg_Cdg", position: {x: 0, y: 0}}, {url: "https://www.youtube.com/watch?v=YiRMs5ZhcH4", position: {x: 0, y: 0}}]);
@@ -10,8 +11,8 @@ export default function IndexPage() {
         const youtubeVideoIndex = e.active.id - 1;
         const youtubeVideo = youtubeVideos[youtubeVideoIndex]
         const occupiedPositions = youtubeVideos.map(youtubeVideo => youtubeVideo.position)
-        const newXPos = youtubeVideo.position.x + (Math.round(e.delta.x/320)*320);
-        const newYPos = youtubeVideo.position.y + (Math.round(e.delta.y/200)*200);
+        const newXPos = youtubeVideo.position.x + (Math.round(e.delta.x/340)*340);
+        const newYPos = youtubeVideo.position.y + (Math.round(e.delta.y/230)*230);
         const positionFree = !occupiedPositions.map(occupiedPosition => occupiedPosition.x == newXPos && occupiedPosition.y == newYPos).includes(true);
         if(positionFree && newXPos >= 0 && newYPos >= 0) {
             setYoutubeVideos(
@@ -24,12 +25,13 @@ export default function IndexPage() {
 
     return (
         <>
+            <HeaderComponent></HeaderComponent>
             <DndContext id="context" onDragEnd={onDragEnd}>
                 {youtubeVideos.map((youtubeVideo, i) => {
                     return <YoutubeVideoComponent video={youtubeVideo} id={i + 1} key={i}></YoutubeVideoComponent>
                 })}
             </DndContext>
-            <PlusCircleIcon className="size-16 fixed bottom-0 right-0"></PlusCircleIcon>
+            <PlusCircleIcon className="size-16 fixed bottom-0 right-0 text-slate-500"></PlusCircleIcon>
         </>
     )
 }
